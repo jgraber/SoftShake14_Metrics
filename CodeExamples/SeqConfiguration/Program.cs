9bfd3b53b190264ce72b0ecc0c1b37c46cf9ea1e
Johnny Graber <jg@jgraber.ch>
2014-10-09 00:48:45 +0200

    add Seq as a sink

--- CodeExamples/SeqConfiguration/Program.cs
+++ CodeExamples/SeqConfiguration/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace SeqConfiguration
     {
         static void Main(string[] args)
         {
+            // Create Logger
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Seq("http://localhost:5341")
+                    .CreateLogger();
 
             LogExamples.WriteLogMessages();
         }

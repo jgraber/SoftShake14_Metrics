374352946b66ac59274051f45c943b6b08ff7e99
Johnny Graber <jg@jgraber.ch>
2014-10-09 00:42:34 +0200

    add project skeleton for Seq

---
+++ CodeExamples/SeqConfiguration/LogExamples.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeqConfiguration
+{
+    public class LogExamples
+    {
+        public static void WriteLogMessages()
+        {
+            Log.Information("A simple info message");
+
+            Log.Debug("Debug is only for internal use");
+
+            Log.Error("Now we have an error!");
+
+
+            var dividend = 10;
+            var divisor = 0;
+
+            try
+            {
+                var result = dividend / divisor;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The division {dividend}/{divisor} failed", dividend, divisor);
+            }
+
+        }
+    }
+}

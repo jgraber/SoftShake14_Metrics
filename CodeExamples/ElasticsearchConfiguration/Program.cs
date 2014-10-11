e9f126febb2e978102b9727a66791b124e3d2e8c
Johnny Graber <jg@jgraber.ch>
2014-10-11 10:04:07 +0200

    add Elasticsearch as a sink

--- CodeExamples/ElasticsearchConfiguration/Program.cs
+++ CodeExamples/ElasticsearchConfiguration/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,13 @@ namespace ElasticsearchConfiguration
         static void Main(string[] args)
         {
 
+            // Create URI to the Elasticsearch server
+            var elasticServer = new Uri("http://localhost:9200");
+
+            // Create Logger
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.ElasticSearch(server: elasticServer)
+                    .CreateLogger();
             LogExamples.WriteLogMessages();
         }
     }

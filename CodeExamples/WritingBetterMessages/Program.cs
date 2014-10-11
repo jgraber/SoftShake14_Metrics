e7ff1594367db28889e91666e14e8c97707e8d00
Johnny Graber <jg@jgraber.ch>
2014-10-11 11:14:32 +0200

    add project skeleton for better log messages

---
+++ CodeExamples/WritingBetterMessages/Program.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritingBetterMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Logger
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Seq("http://localhost:5341")
+                    .CreateLogger();
+        }
+    }
+}

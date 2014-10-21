6a6a3d573914af37842a3d173ec19ce522aedf46
Johnny Graber <jg@jgraber.ch>
2014-10-21 20:07:51 +0200

    add RavenDB as a sink

--- CodeExamples/RavenDBConfiguration/Program.cs
+++ CodeExamples/RavenDBConfiguration/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Raven.Client.Document;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,19 @@ namespace RavenDBConfiguration
     {
         static void Main(string[] args)
         {
+            // Create document store
+            var documentStore = new DocumentStore
+            {
+                Url = "http://localhost:8080",
+                DefaultDatabase = "Logs"
+            };
+            documentStore.Initialize();
+
+
+            // Create Logger
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.RavenDB(documentStore)
+                    .CreateLogger();
 
             LogExamples.WriteLogMessages();
         }
